c3f191b81eb6252537ab22e312047381feb62f86
Richard Henwood <rbh288@cornell.edu>
2017-05-04 10:19:39 -0400

    Write more methods for 'getting' and calling useful opengl funcs

--- src/backends/opengl.ts
+++ src/backends/opengl.ts
@@ -3,6 +3,137 @@ import * as llvm from '../../node_modules/llvmc/src/wrapped';
 import { ASTVisit, ast_visit, compose_visit } from '../visit';
 import * as ast from '../ast';
 
+//////////////////////////////////////////
+// useful openGL bindings
+//////////////////////////////////////////
+
+/**
+ * Methods for getting various openGL types
+ */
+function glchar() {return llvm.IntType.int8();}
+function glstring() {return llvm.PointerType.create(glchar(), 0);}
+function glint() {return llvm.IntType.int32();}
+function gluint() {return llvm.IntType.int32();}
+function glsizei() {return llvm.IntType.int32();}
+function glenum() {return llvm.IntType.int32();}
+
+/*
+ * GLuint glCreateShader(GLenum shaderType)
+ */
+function glCreateShader(emitter: llvm_be.LLVMEmitter, shader_type: llvm.Value): llvm.Value {
+  let func: llvm.Function = emitter.mod.getFunction("glCreateShader");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = gluint();
+    let arg_types: llvm.Type[] = [glenum()];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glCreateShader", func_type);
+  }
+  return emitter.builder.buildCall(func, [shader_type], "");
+}
+
+/*
+ * void glShaderSource(GLuint shader, GLsizei count, const GLchar **string, const GLint *length)
+ */
+function glShaderSource(emitter: llvm_be.LLVMEmitter, shader: llvm.Value, count: llvm.Value, sources: llvm.Value, length: llvm.Value) {
+  let func: llvm.Function = emitter.mod.getFunction("glShaderSource");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = llvm.VoidType.create();
+    let arg_types: llvm.Type[] = [gluint(), glsizei(), llvm.PointerType.create(glstring(), 0), llvm.PointerType.create(glint(), 0)];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glShaderSource", func_type);
+  } 
+  return emitter.builder.buildCall(func, [shader, count, sources, length], "");
+}
+
+/*
+ * void glCompileShader(  GLuint shader)
+ */
+function glCompileShader(emitter: llvm_be.LLVMEmitter, shader_type: llvm.Value) {
+  let func: llvm.Function = emitter.mod.getFunction("glCompileShader");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = llvm.VoidType.create();
+    let arg_types: llvm.Type[] = [gluint()];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glCompileShader", func_type);
+  }
+  return emitter.builder.buildCall(func, [shader_type], "");
+}
+
+function glGetShaderParameter(emitter: llvm_be.LLVMEmitter) {
+  throw "not implemented yet";
+}
+
+function glGetShaderInfoLog(emitter: llvm_be.LLVMEmitter) {
+  throw "not implemented yet";
+}
+
+/*
+ * GLuint glCreateProgram(void)
+ */
+function glCreateProgram(emitter: llvm_be.LLVMEmitter) {
+  let func: llvm.Function = emitter.mod.getFunction("glCreateProgram");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = gluint();
+    let arg_types: llvm.Type[] = [];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glCreateProgram", func_type);
+  }
+  return emitter.builder.buildCall(func, [], "");
+}
+
+/*
+ * void glAttachShader(GLuint program, GLuint shader)
+ */
+function glAttachShader(emitter: llvm_be.LLVMEmitter, program: llvm.Value, shader: llvm.Value) {
+  let func: llvm.Function = emitter.mod.getFunction("glAttachShader");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = llvm.VoidType.create();
+    let arg_types: llvm.Type[] = [gluint(), gluint()];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glAttachShader", func_type);
+  }
+  return emitter.builder.buildCall(func, [program, shader], "");
+}
+
+/*
+ * void glLinkProgram(GLuint program)
+ */
+function glLinkProgram(emitter: llvm_be.LLVMEmitter, program: llvm.Value) {
+  let func: llvm.Function = emitter.mod.getFunction("glLinkProgram");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = llvm.VoidType.create();
+    let arg_types: llvm.Type[] = [gluint()];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glLinkProgram", func_type);
+  }
+  return emitter.builder.buildCall(func, [program], "");
+}
+
+function glGetProgramParameter(emitter: llvm_be.LLVMEmitter) {
+  throw "not implemented yet";
+}
+
+/*
+ * void glGetProgramInfoLog(  GLuint program, GLsizei maxLength, GLsizei *length, GLchar *infoLog)
+ */
+function glGetProgramInfoLog(emitter: llvm_be.LLVMEmitter, program: llvm.Value, max_length: llvm.Value, length: llvm.Value, info_log: llvm.Value) {
+  let func: llvm.Function = emitter.mod.getFunction("glGetProgramInfoLog");
+  if (func.ref.isNull()) {
+    let ret_type: llvm.Type = llvm.VoidType.create();
+    let arg_types: llvm.Type[] = [gluint(), glsizei(), llvm.PointerType.create(glsizei(),0), glstring()];
+    let func_type: llvm.FunctionType = llvm.FunctionType.create(ret_type, arg_types);
+    func = emitter.mod.addFunction("glGetProgramInfoLog", func_type);
+  }
+  return emitter.builder.buildCall(func, [program, max_length, length, info_log], "");
+}
+
+//////////////////////////////////////////
+
+function compile_glsl(emitter: llvm_be.LLVMEmitter, shader_type: number, src) {
+  // let shader: llvm.Value = glCreateShader(emitter, llvm.ConstInt.create(shader_type, llvm.IntType.int32()));
+  throw "not implemented yet";
+}
+
 // Extend the JavaScript compiler with some WebGL specifics.
 let compile_rules: ASTVisit<llvm_be.LLVMEmitter, llvm.Value> =
   compose_visit(llvm_be.compile_rules, {
